6d0e78767b417a02390c5b9236b156ba6b3ff1fd
ashtonritchie <95386885+ashtonritchie@users.noreply.github.com>
2021-12-01 15:08:41 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import pyautogui as gui
+import streamlit as st
+import time
+
+
+st.set_page_config(page_title="StayGreen", layout='wide')
+
+st.write('''
+    # Welcome to StayGreen!
+    a simple solution for appearing online, offline
+''')
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    with st.expander('Instructions', expanded=False):
+        st.write('''
+        To begin, simply click "Begin Session." Then, move your mouse to a scrollable window. I like to use a
+        Teams chat. Remove your hand from the mouse and watch your window scroll up and down in perpetuity.
+        Your online status will appear active to anyone who my be watching. When it's time to return to work, 
+        click "Resume Work." Enjoy!
+             ''')
+
+speed_down = -100
+sleeptime = 1
+speed_up = 100
+gui.FAILSAFE = False
+start = st.button('Begin Session')
+stop = st.button('Resume Work')
+
+if start:
+    st.markdown('<font color=‘red’>Session in progress...</font>', unsafe_allow_html=True)
+    while start:
+        gui.scroll(int(speed_down))
+        gui.sleep(int(sleeptime))
+        gui.scroll(int(speed_up))
+        gui.sleep(int(sleeptime))
+        if stop:
+            break
+        st.write('')
+
+
+
+
+
+
